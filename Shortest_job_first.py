15f95610827ba2f61212ed6c9e0b0f88a2781ae0
jaganmohanchari <ekabathulajaganmohanchari@gmail.com>
2024-07-06 10:05:57 +0530

    Shortest_Job_First_Solved_In _Python

---
+++ Shortest_job_first.py
@@ -0,0 +1,54 @@
+
+#GeeksForGeeks Problem
+
+#link is https://www.geeksforgeeks.org/problems/shortest-job-first/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=shortest-job-first
+
+'''
+Geek is a software engineer. He is assigned with the task of calculating average waiting time of all the processes by following shortest job first policy.
+
+The shortest job first (SJF) or shortest job next, is a scheduling policy that selects the waiting process with the smallest execution time to execute next.
+
+Given an array of integers bt of size n. Array bt denotes the burst time of each process. Calculate the average waiting time of all the processes and return the nearest integer which is smaller or equal to the output.
+
+Note: Consider all process are available at time 0.
+
+Example 1:
+
+Input:
+n = 5
+bt = [4,3,7,1,2]
+Output: 4
+Explanation: After sorting burst times by shortest job policy, calculated average waiting time is 4.
+Example 2:
+
+Input:
+n = 4
+arr = [1,2,3,4]
+Output: 2
+Explanation: After sorting burst times by shortest job policy, calculated average waiting time is 2.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function solve() which takes bt[] as input parameter and returns the average waiting time of all the processes.
+
+Expected Time Complexity: O(nlog(n))
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= n <= 105
+1 <= arr[i] <= 105
+'''
+
+
+#Solution 
+
+bt=list(map(int,input().split()))
+list_a=sorted(bt)
+sjf=0 
+processor=0
+for i in range(len(list_a)):
+    sjf=sjf+processor 
+    processor=processor+list_a[i] 
+result=sjf//len(list_a)
+print(result)
+#inputs
+#1234
+#14327
